c1c9a97363560669d84dc9e0d388163d54f7e83e
3amo-magdy <70772544+3amo-magdy@users.noreply.github.com>
2021-01-26 23:16:13 -0200

    Update main.c

--- main.c
+++ main.c
@@ -1,38 +1,15 @@
-#include "headchess.h"
-int main(){
-    printf(" .......                                ........\n");
-    printf("|             |                              | made with love |\n");
-    printf("|    Chess    |                              |       in       |\n");
-    printf("|             |                              |      Egypt     |\n");
-    printf(" .-.-.-.-.-.-.                                .-.-.-.-.-.-.-.-\n");
-    int state;
-    // set turn && max_turn according to what was chosen : either [loaded game] OR [new game]
-    int turn =0;
-    menu(&turn);
-    while(1){
-        state=play(turn);
-        if(state==2){
-            turn--;
-        }
-        else if(state==3){
-            turn++;
-        }
-        else if(!state){
-            char t;
-            printf("game ended, however you can undo, type:\n['u' to undo]\n['y' to go back]\n[any button else to quit]\n");
-            scanf("%c",&t);
-            if(t=='u'){
-                turn--;
-            }
-            else if(t=='y'){
-                //go back to menu
-            }
-            else{
-                break;
-            }
-        }
-        if(state==5){
-            turn++;
-        }
-    }
+#include"play.h"
+int main(int argc,char**argv){
+    int turn=0;
+    main_menu(&turn);
+    //sdl initialization
+    SDL_Window*x=NULL;
+    init_window(x);
+
+    SDL_Surface* screenSurface = NULL;
+    gameplay(screenSurface,turn,x);
+
+    //de-initialization
+    end_window();
+    return 0;
 }
